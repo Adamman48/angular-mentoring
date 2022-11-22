14d5e9f8a248dfda8dbb99c2d8a586337396c922
Adamman48 <adam.piskynagy@gmail.com>
2022-11-22 14:58:47 +0100

    fix: fixed linter rule for custom prefix

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageComponent } from './page/page.component';
+import { CourseItemComponent } from '../courses/course-item/course-item.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    PageComponent,
+    CourseItemComponent,
+  ],
+  imports: [CommonModule],
+})
+export class CoreModule {}
---
+++ src/app/core/definitions/user.core.ts
@@ -0,0 +1,5 @@
+export interface UserEntityInterface {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
