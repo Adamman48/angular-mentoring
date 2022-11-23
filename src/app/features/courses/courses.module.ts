0ba72ef62295c1d10c48cb799e4e61427ef6521b
Adamman48 <adam.piskynagy@gmail.com>
2022-11-23 14:27:41 +0100

    feat: initialized courses page component

--- src/app/features/courses/courses.module.ts
+++ src/app/features/courses/courses.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoursesComponent } from './courses-page/courses.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { CourseItemComponent } from './course-item/course-item.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [CoursesComponent, CourseItemComponent],
+  imports: [CommonModule, SharedModule],
 })
 export class CoursesModule {}
