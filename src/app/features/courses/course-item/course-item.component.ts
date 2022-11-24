87084581197dd8551d7c4d5c79470bf463a8e42c
Adamman48 <adam.piskynagy@gmail.com>
2022-11-24 10:47:42 +0100

    feat: initialized courseItem

--- src/app/features/courses/course-item/course-item.component.ts
+++ src/app/features/courses/course-item/course-item.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { CourseItemInterface } from 'src/app/core/definitions/courses.feature';
 
 @Component({
   selector: 'trng-course-item',
   templateUrl: './course-item.component.html',
   styleUrls: ['./course-item.component.scss'],
 })
-export class CourseItemComponent {}
+export class CourseItemComponent implements CourseItemInterface {
+  id = 'staticId';
+  title = 'static title';
+  creationDate: Date = new Date();
+  duration = 60;
+  description = 'static description';
+}
