c018bede2205eca3368f536b0cee68b4bc584bc4
Adamman48 <adam.piskynagy@gmail.com>
2022-12-01 00:50:35 +0100

    feat: added modal closing logic

--- src/app/core/modal/modal.component.ts
+++ src/app/core/modal/modal.component.ts
@@ -20,6 +20,7 @@ export class ModalComponent {
     } else if (callback && callbackArgs.length) {
       callback(...callbackArgs);
     }
+    this.handleCancelBtnClick();
   }
 
   handleCancelBtnClick() {
