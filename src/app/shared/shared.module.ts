434e7e32ab162f634e6af2bb5d0f0ada2f29f956
Adamman48 <adam.piskynagy@gmail.com>
2022-11-30 17:11:57 +0100

    fix: removed redundant import

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,6 @@ import { ButtonComponent } from './components/button/button.component';
 import { SearchComponent } from './components/search/search.component';
 import { FormsModule } from '@angular/forms';
 import { IconComponent } from './components/icon/icon.component';
-import { SearchFilterPipe } from './components/search/search-filter.pipe';
 
 @NgModule({
   declarations: [
