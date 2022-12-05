b82724e86e2d89b4b881b3b4a294b0ff13bba158
Adamman48 <adam.piskynagy@gmail.com>
2022-12-05 11:37:46 +0100

    feat: added styling for login component and refactored breadcrumbs style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { LoginModule } from './features/login/login.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, LoginModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/features/login/login.module.ts
+++ src/app/features/login/login.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [LoginPageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
+  exports: [LoginPageComponent],
 })
 export class LoginModule {}
